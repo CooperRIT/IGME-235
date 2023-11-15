40c554cb669f3bfaebafe9ee63c0d33efed47f6a
Cooper Shropshire (RIT Student) <112188791+CooperRIT@users.noreply.github.com>
2023-11-15 14:52:34 -0500

    set up the inital base for project 2

--- project2/js/script.js
+++ project2/js/script.js
@@ -6,10 +6,13 @@ function getPokemon()
 {
     let randomValue = Math.floor(Math.random() * 700);
     const SERVICE_URL = `https://pokeapi.co/api/v2/pokemon-form/${randomValue}`
+    //const SPRITE_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomValue}.png`
 
     let url = SERVICE_URL;
+    //let spriteURL = SPRITE_URL;
 
     getData(url);
+    //getData(spriteURL);
 }
 
 function getData(url)
@@ -31,6 +34,7 @@ function dataLoaded(e)
     let obj = JSON.parse(xhr.responseText);
 
     document.querySelector("#pokemon").innerHTML = obj.name;
+    //document.querySelector("#center-image").src = obj;
 }
 
 function dataError()
