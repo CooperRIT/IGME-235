2e9f46108498ac4b46db6a0f758e899558cd542e
CooperRIT <112188791+CooperRIT@users.noreply.github.com>
2023-11-17 01:29:00 -0500

    started project 2

--- project2/js/script.js
+++ project2/js/script.js
@@ -1,43 +1,152 @@
-window.onload = getPokemon();
+window.onload = getStoredData;
+let pokemonName = "";
+let wrongPokemonName = [];
 
-let displayTerm = "";
+let intCurrentScore = 0;
+let intBestScore = 0;
+
+let wrongCases = ["pika", "kogy", "bob", "flippy"];
+
+let storedIntBest = localStorage.getItem("intBestScore");
+
+document.addEventListener('DOMContentLoaded', getPokemon());
+
+function getStoredData()
+{
+    if (storedIntBest)
+    {
+        console.log(storedIntBest);
+        intBestScore = storedIntBest;
+        document.querySelector("#left-text").innerHTML = "Best Score: " + intBestScore;
+    }
+}
 
 function getPokemon()
 {
     let randomValue = Math.floor(Math.random() * 700);
-    const SERVICE_URL = `https://pokeapi.co/api/v2/pokemon-form/${randomValue}`
-    //const SPRITE_URL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomValue}.png`
 
-    let url = SERVICE_URL;
-    //let spriteURL = SPRITE_URL;
+    wrongPokemonName = [];
 
-    getData(url);
-    //getData(spriteURL);
+    const urls = [];
+
+    //Pushes the right button
+    urls.push(`https://pokeapi.co/api/v2/pokemon/${randomValue}`);
+
+    for (let i = 0; i < 3; i++)
+    {
+        urls.push(`https://pokeapi.co/api/v2/pokemon/${Math.floor(Math.random() * 700)}`);
+    }
+    //const SERVICE_URL = `https://pokeapi.co/api/v2/pokemon/${randomValue}`;
+
+    //let url = SERVICE_URL;
+
+    getData(urls);
 }
 
-function getData(url)
+function getData(urls)
 {
-	let xhr = new XMLHttpRequest();
+    let tempIncrement = 1;
+    while (tempIncrement <= 3)
+    {
+        let xhr = new XMLHttpRequest();
 
-	xhr.onload = dataLoaded;
+	    xhr.onload = dataLoadedIncorrectly;
+
+	    xhr.onerror = dataError;
+
+	    xhr.open("GET", urls[tempIncrement]);
+        tempIncrement++;
+	    xhr.send();
+    }
+
+    let xhr = new XMLHttpRequest();
+
+	xhr.onload = dataLoadedCorrectly;
 
 	xhr.onerror = dataError;
 
-	xhr.open("GET", url);
+	xhr.open("GET", urls[0]);
 	xhr.send();
 }
 
-function dataLoaded(e)
+function dataLoadedCorrectly(e)
 {
 	let xhr = e.target;
 
     let obj = JSON.parse(xhr.responseText);
 
-    document.querySelector("#pokemon").innerHTML = obj.name;
-    //document.querySelector("#center-image").src = obj;
+    pokemonName = obj.name;
+    
+    document.querySelector("#center-image").src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${obj.id}.png`;
+
+    console.log(wrongPokemonName);
+    
+    assignButtons();
+
+    console.log("CompletedCorrectly");
+}
+
+function dataLoadedIncorrectly(e)
+{
+    let xhr = e.target;
+
+    let obj = JSON.parse(xhr.responseText);
+
+    wrongPokemonName.push(obj.name);
+    
+    console.log("Completed");
 }
 
 function dataError()
 {
+    console.log("oops");
+}
 
+function assignButtons()
+{
+    // Your JavaScript code here
+    var buttons = document.querySelectorAll('button');
+
+    let randomValue = Math.floor(Math.random() * buttons.length);
+
+    let incrementedWrong = 0;
+    
+    for (let i = 0; i < buttons.length; i++) 
+    {
+        if (i == randomValue)
+        {
+            buttons[i].removeEventListener('click', wrongButton);
+            buttons[i].addEventListener('click', rightButton);
+            buttons[i].textContent = pokemonName;
+
+        }
+        else
+        {
+            buttons[i].removeEventListener('click', rightButton);
+            buttons[i].addEventListener('click', wrongButton);
+            buttons[i].textContent = wrongPokemonName[incrementedWrong] != undefined ? wrongPokemonName[incrementedWrong] : wrongCases[i];
+            incrementedWrong++;
+        }
+    }
+}
+
+function wrongButton()
+{
+    if (intCurrentScore > intBestScore)
+    {
+        intBestScore = intCurrentScore;
+        
+        localStorage.setItem("intBestScore", intBestScore);
+        document.querySelector("#left-text").innerHTML = "Best Score: " + intBestScore;
+    }
+    intCurrentScore = 0;
+    document.querySelector("#right-text").innerHTML = "Current Score: " + intCurrentScore;
+    getPokemon();
+}
+
+function rightButton()
+{
+    intCurrentScore++;
+    document.querySelector("#right-text").innerHTML = "Current Score: " + intCurrentScore;
+    getPokemon();
 }
